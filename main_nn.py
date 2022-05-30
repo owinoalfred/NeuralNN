47417262ed7fdf27e89cd50990ccfc452a556524
alfredo <ogzialfred@gmail.com>
2022-05-30 12:35:06 +0300

    importing the needed resources

--- main_nn.py
+++ main_nn.py
@@ -1,6 +1,20 @@
-from yahoo_finance import Share
-yahoo = Share('YHOO')
+import pandas as pd
+import yfinance as yf
+import datetime
+from datetime import date, timedelta
+today = date.today()
 
+d1 = today.strftime("%Y-%m-%d")
+end_date = d1
+d2 = date.today() - timedelta(days=5000)
+d2 = d2.strftime("%Y-%m-%d")
+start_date = d2
 
-
-print (yahoo.get_open())
+data = yf.download('NFLX', 
+                      start=start_date, 
+                      end=end_date, 
+                      progress=False)
+data["Date"] = data.index
+data = data[["Date", "Open", "High","Low", "Close", "Adj Close", "Volume"]]
+data.reset_index(drop=True, inplace=True)
+print(data.tail()) 
